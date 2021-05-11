280cbb18bcb20d96403e91a5d26fa8b326d02a04
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-11 12:48:31 +0200

    js01

--- src/js/01main.js
+++ src/js/01main.js
@@ -1,23 +1,10 @@
-/*  SOLUCION D ELUCIA  <!DOCTYPE html>
-
-  <body>
-    <button class="button1"><i class="fas fa-chevron-down fa-chevron-up arrow"></i></button>
-    <div class="js-container1 container1">
-      <p>
-        Lorerit viciis consectetur esse iure, omnis dignissimos
-        ut natus?
-      </p>
-    </div>
-    </div>
-    <script src="main.js"></script>
-  </body>
-</html>  
+/*
 "use strict";
-const container = document.querySelector(".colors_line");
-const active = document.querySelector(".b");
+const choosec1 = document.querySelector(".colors_title");
+const choosec2 = document.querySelector(".");
 function evenT() {
-  container.classList.toggle("collapse");
-  active.classList.toggle("arrow");
+  choosec1.classList.toggle("collapse");
+  choosec2.classList.toggle("arrow");
 }
 active.addEventListener("click", evenT);
 
