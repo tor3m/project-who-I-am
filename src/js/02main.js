ed5f5b609bfc05802dd9db30eb699946c5550f8a
Lucia Cano <luciacanoras@gmail.com>
2021-05-11 19:41:25 +0200

    Add funtional partials js

---
+++ src/js/02main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const detailsForm = document.querySelector(".details__form");
+const active = document.querySelector(".buton");
+
+function evenT() {
+  detailsForm.classList.toggle("collapse");
+  active.classList.toggle("details-up");
+}
+active.addEventListener("click", evenT);
+//
