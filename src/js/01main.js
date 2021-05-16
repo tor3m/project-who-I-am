3ab3e24a8cd144c8b43181205b913a0a98206cac
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-16 15:47:58 +0200

    collapsable1

--- src/js/01main.js
+++ src/js/01main.js
@@ -1,11 +1,10 @@
-//
+
 // "use strict";
 // const choosec1 = document.querySelector(".js-colorea");
-// const choosec2 = document.querySelector(".");
-// function evenT() {
-//   choosec1.classList.toggle("collapse");
-//   choosec2.classList.toggle("arrow");
+// const choosec2 = document.querySelector(".colors-up");
+// function eventFill() {
+//   choosec1.classList.toggle("collapse2");
+//   choosec2.classList.toggle("arrow2");
 // }
-// active.addEventListener("click", evenT);
+// active.addEventListener("click", eventfill);
 
-//
