998512eb5b2249ffccb80fe13dc44f4030b6b755
Lucia Cano <luciacanoras@gmail.com>
2021-05-17 16:29:15 +0200

    Repair funtions collapse

--- src/js/00-01Collapse.js
+++ src/js/00-01Collapse.js
@@ -3,11 +3,17 @@
 function eventFill() {
   chooseC.classList.toggle("collapseShare");
   choosec2.classList.toggle("arrowShare");
+}
+
+function eventFill2() {
   detailsForm.classList.toggle("collapseShare");
   active.classList.toggle("arrowShare");
+}
+
+function eventFill3() {
   containerShare.classList.toggle("collapseShare");
   activeShare.classList.toggle("arrowShare");
 }
 choosec2.addEventListener("click", eventFill);
-active.addEventListener("click", eventFill);
-activeShare.addEventListener("click", eventFill);
+active.addEventListener("click", eventFill2);
+activeShare.addEventListener("click", eventFill3);
