f18b140e9944699bfb9f3d36d325a5758c430c1d
Lucia Cano <luciacanoras@gmail.com>
2021-05-19 21:27:58 +0200

    Add partial js colorsForm

---
+++ src/js/00-03ColorsForm.js
@@ -0,0 +1,5 @@
+// let photoPreviewFooter = document.querySelector("js-footerpreview");
+// let photoPreviewHeader = document.querySelector(".js-headerpreview");
+
+// // photoPreviewHeader.style.cssText = "color:red, border:purple";
+// photoPreviewFooter.style.cssText = "color:red, border:purple";
