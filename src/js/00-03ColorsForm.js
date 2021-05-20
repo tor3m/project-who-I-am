3b817bdfdbc374c9a370f4b6a708a2799f64e624
Lucia Cano <luciacanoras@gmail.com>
2021-05-20 10:08:46 +0200

    Add classes to add preview color

--- src/js/00-03ColorsForm.js
+++ src/js/00-03ColorsForm.js
@@ -3,3 +3,7 @@
 
 // // photoPreviewHeader.style.cssText = "color:red, border:purple";
 // photoPreviewFooter.style.cssText = "color:red, border:purple";
+
+const borderColor = document.querySelector("js-headerpreview");
+const textColor = document.querySelector("js-name");
+const links2 = document.querySelector("js-footerpreview");
