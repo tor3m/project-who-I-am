f584818a55c29658c1f4a1c6eedfec10b67ded26
paulaavn <paulaalbovn@gmail.com>
2021-05-20 16:07:35 +0200

    add p

--- src/js/00-03ColorsForm.js
+++ src/js/00-03ColorsForm.js
@@ -4,6 +4,6 @@
 // // photoPreviewHeader.style.cssText = "color:red, border:purple";
 // photoPreviewFooter.style.cssText = "color:red, border:purple";
 
-const borderColor = document.querySelector("js-headerpreview");
-const textColor = document.querySelector("js-name");
-const links2 = document.querySelector("js-footerpreview");
+const borderColor = document.querySelector(".js-headerpreview");
+const textColor = document.querySelector(".js-name");
+const links2 = document.querySelector(".js-footerpreview");
